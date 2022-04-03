e9e52e838486581bb364768bdc403df02ad8ceed
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2022-04-03 22:17:17 +0000

    rows and columns

--- rcApp/Pages/Index.cshtml.cs
+++ rcApp/Pages/Index.cshtml.cs
@@ -54,12 +54,7 @@ public class IndexModel : PageModel
         Cocktails.Add(new Cocktail() { Id = 1004, Name = "Beer", Price = 15.25M, ImagePath = "images/NoImage.jpg" });
         Cocktails.Add(new Cocktail() { Id = 1005, Name = "Hand Craft", Price = 15.25M, ImagePath = "https://user-images.githubusercontent.com/41929050/61567052-13938600-aa33-11e9-9a88-cd842073ba44.jpg" });
         Cocktails.Add(new Cocktail() { Id = 1006, Name = "Pink shirt", Price = 15.25M, ImagePath = "https://user-images.githubusercontent.com/41929050/61567062-14c4b300-aa33-11e9-9dcd-8bfed4ece810.png" });
-        Cocktails.Add(new Cocktail() { Id = 1001, Name = "Grasshopper", Price = 15.25M, ImagePath = "images/grasshopper.jpg" });        
-        Cocktails.Add(new Cocktail() { Id = 1002, Name = "Manhattan", Price = 15.25M, ImagePath = "images/manhattan.jpg" });
-        Cocktails.Add(new Cocktail() { Id = 1003, Name = "Martini", Price = 15.25M, ImagePath = "images/martini.jpg" });            
-        Cocktails.Add(new Cocktail() { Id = 1004, Name = "Beer", Price = 15.25M, ImagePath = "images/NoImage.jpg" });
-        Cocktails.Add(new Cocktail() { Id = 1005, Name = "Hand Craft", Price = 15.25M, ImagePath = "https://user-images.githubusercontent.com/41929050/61567052-13938600-aa33-11e9-9a88-cd842073ba44.jpg" });
-        Cocktails.Add(new Cocktail() { Id = 1006, Name = "Pink shirt", Price = 15.25M, ImagePath = "https://user-images.githubusercontent.com/41929050/61567062-14c4b300-aa33-11e9-9dcd-8bfed4ece810.png" });
+        
         // 
 
 
